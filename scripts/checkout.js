b714766334827434b1d47327364cd5958a130638
Shayan Hussain <shayanhusayn1@gmail.com>
2025-07-20 07:10:17 +0500

    Make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {cart, removeFromCart, updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -107,7 +107,9 @@ function deliveryOptionsHTML(matchingProduct,cartItem){
     const priceString = 
     deliveryOption.priceCents === 0? 'FREE': `$${formatCurrency(deliveryOption.priceCents)} -`;
     html +=  `
-     <div class="delivery-option">
+     <div class="delivery-option js-delivery-option"
+     data-product-id="${matchingProduct.id}"
+     data-delivery-option-id="${deliveryOption.id}">
         <input type="radio"
           ${isChecked ? 'checked': ''}
           class="delivery-option-input"
@@ -141,5 +143,12 @@ function deliveryOptionsHTML(matchingProduct,cartItem){
         `.js-cart-item-container-${productId}`
       );
       container.remove();
+    });
+  });
+
+  document.querySelectorAll('.js-delivery-option').forEach((element)=>{
+    element.addEventListener('click', ()=>{
+      const {productId, deliveryOptionId} = element.dataset;
+      updateDeliveryOption(productId,deliveryOptionId);
     });
   });
